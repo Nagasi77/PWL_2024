c196b32829e5cf9730e0a904701fb6f4c1180c46
Much. Zannur Nadhif <muchzannurnadhif@gmail.com>
2025-02-17 17:39:32 +0700

    Jobsheet2-SoalPraktikum

--- POS/routes/web.php
+++ POS/routes/web.php
@@ -1,6 +1,22 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\ProductController;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\SalesController;
+
+Route::get('/home', HomeController::class);
+
+// Prefix untuk kategori produk
+Route::prefix('category')->group(function () {
+    Route::get('/{type}', [ProductController::class, 'category']);
+});
+
+// Parameter untuk user
+Route::get('/user/{id}/name/{name}', [UserController::class, 'profile']);
+
+Route::get('/sales', SalesController::class);
 
 /*
 |--------------------------------------------------------------------------
